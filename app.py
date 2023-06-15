2163ad99c6e00bd59123f45e42f44d63a35f4d34
Jordan James <jaj9608@mavs.uta.edu>
2023-06-15 00:53:24 -0500

    comments

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+#Programming Assignment 2
+#Jordan James 1001879608
+#CSE 6332-002
+
 import math
 import pandas as pd
 import os, uuid
@@ -8,9 +12,8 @@ from flask import (Flask, redirect, render_template, request,
 
 app = Flask(__name__)
 
-
+# Setting up initial parameters
 typ = "'earthquake'"
-
 r = 5000
 f = 40
 l = 1
@@ -21,6 +24,7 @@ nf = f+ns
 wl = l-ew
 el = l+ew
 
+# Setting up database connection parameters
 server = 'q2server.mysql.database.azure.com'
 database = 'testdb'
 username = 'servadmin'
@@ -44,9 +48,11 @@ def favicon():
 
 @app.route('/location', methods=['POST'])
 def location():
+    # Retrieving data from the request form
     arg = request.form.get('name')
     args = arg.split(',')
     
+    # Calculating latitude and longitude boundaries
     r = float(args[2])
     f = float(args[0])
     l = float(args[1])
@@ -57,6 +63,7 @@ def location():
     wl = l-ew
     el = l+ew
     
+    # SQL query to retrieve earthquakes within the specified boundaries
     query = '''SELECT *
     FROM earthquakes
     WHERE latitude BETWEEN {} AND {}
@@ -69,6 +76,7 @@ def location():
     dfinish = "'2023-05-10T23:20:14.660Z'" # Enter your desired start date/time in the string
     dstart = "'2023-05-10T20:20:14.660Z'"
     
+    # Executing the SQL query based on different boundary scenarios
     if sf < nf and el < wl:
         result = pd.read_sql_query(query.format(sf,nf,el,wl, typ), engine)
     elif sf < nf and wl < el:
@@ -78,7 +86,7 @@ def location():
     elif nf < sf and el < wl:
         result = pd.read_sql_query(query.format(nf,sf,el,wl, typ), engine)          
         
-        
+    # Rendering the template with the query result
     if result is not None:
        return render_template('count.html', name = len(result), tables=[result.to_html(classes='data', header="true")])
     else:
@@ -87,9 +95,11 @@ def location():
 
 @app.route('/range', methods=['POST'])
 def range():
+    # Retrieving data from the request form
     arg = request.form.get('name')
     args = arg.split(',')
     
+    # SQL query to retrieve earthquakes within the specified time and magnitude range
     query = '''SELECT *
     FROM earthquakes
     WHERE time BETWEEN {} AND {}
@@ -100,17 +110,19 @@ def range():
     dfinish = "'2023-05-10T23:20:14.660Z'" # Enter your desired start date/time in the string
     dstart = "'2023-05-10T20:20:14.660Z'"
     
+    # Executing the SQL query
     result = pd.read_sql_query(query.format(args[0], args[1], float(args[2]), float(args[3]), typ), engine)
     
+    # Rendering the template with the query result
     if result is not None:
        return render_template('count.html', name = len(result), tables=[result.to_html(classes='data', header="true")])
     else:
        print('Request for count page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
-
 @app.route('/count', methods=['POST'])
 def count():
+    # SQL query to retrieve earthquakes of a specific type and magnitude greater than a threshold
     query = '''SELECT *
     FROM earthquakes
     WHERE type = {}
@@ -120,12 +132,13 @@ def count():
     result = None
     arg = request.form.get('name')
     result = pd.read_sql_query(query.format(typ, float(arg)), engine)
+    
+    # Rendering the template with the query result
     if result is not None:
        return render_template('count.html', name = len(result), tables=[result.to_html(classes='data', header="true")])
     else:
        print('Request for count page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
    app.run()
