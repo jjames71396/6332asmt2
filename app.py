b24732a8f538e6b36a96a978d58364931e60a87d
Jordan James <jaj9608@mavs.uta.edu>
2023-06-20 19:59:20 -0500

    first commit

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import pandas as pd
 import os, uuid
 from sqlalchemy import create_engine
 import pymysql
+import time
+import redis
+import random
+import pyarrow as pa
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
@@ -23,6 +27,11 @@ sf = f-ns
 nf = f+ns
 wl = l-ew
 el = l+ew
+
+r = redis.Redis(
+    host='6332cache.redis.cache.windows.net',
+    port=6379, 
+    password='O03L4nySbOtQY2Qfr7qiRIBi8EwmtQ7D5AzCaJZK7ZA=')
 
 # Setting up database connection parameters
 server = 'q2server.mysql.database.azure.com'
@@ -51,12 +60,13 @@ def location():
     # Retrieving data from the request form
     arg = request.form.get('name')
     args = arg.split(',')
-    
+    if len(args) < 4:
+        return redirect(url_for('index'))
     # Calculating latitude and longitude boundaries
-    r = float(args[2])
+    rd = float(args[2])
     f = float(args[0])
     l = float(args[1])
-    ns = r/111        # North-south distance in degrees (69 for miles, 111 for km)
+    ns = rd/111        # North-south distance in degrees (69 for miles, 111 for km)
     ew = ns / math.cos(f) # East-west distance in degrees
     sf = f-ns
     nf = f+ns
@@ -75,47 +85,67 @@ def location():
     result = None
     dfinish = "'2023-05-10T23:20:14.660Z'" # Enter your desired start date/time in the string
     dstart = "'2023-05-10T20:20:14.660Z'"
-    
-    # Executing the SQL query based on different boundary scenarios
-    if sf < nf and el < wl:
-        result = pd.read_sql_query(query.format(sf,nf,el,wl, typ), engine)
-    elif sf < nf and wl < el:
-        result = pd.read_sql_query(query.format(sf,nf,wl,el, typ), engine)
-    elif nf < sf and wl < el:
-        result = pd.read_sql_query(query.format(nf,sf,wl,el, typ), engine)
-    elif nf < sf and el < wl:
-        result = pd.read_sql_query(query.format(nf,sf,el,wl, typ), engine)          
-        
+    times = []
+    key = str(f)+str(l)+str(rd)
+    for i in range(int(args[3])):
+        start = time.time()
+        result = pa.default_serialization_context()
+        res = r.get(key)
+        if res is not None:
+            result.deserialize(res)
+        else:
+            # Executing the SQL query based on different boundary scenarios
+            if sf < nf and el < wl:
+                result = pd.read_sql_query(query.format(sf,nf,el,wl, typ), engine)
+            elif sf < nf and wl < el:
+                result = pd.read_sql_query(query.format(sf,nf,wl,el, typ), engine)
+            elif nf < sf and wl < el:
+                result = pd.read_sql_query(query.format(nf,sf,wl,el, typ), engine)
+            elif nf < sf and el < wl:
+                result = pd.read_sql_query(query.format(nf,sf,el,wl, typ), engine)   
+            r.set(key,pa.default_serialization_context().serialize(result).to_buffer().to_pybytes())
+        finish = time.time()    
+        times.append(str(key)+": "+str(finish-start))
     # Rendering the template with the query result
     if result is not None:
-       return render_template('count.html', name = len(result), tables=[result.to_html(classes='data', header="true")])
+       return render_template('count.html', name = times)
     else:
        print('Request for count page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
-@app.route('/range', methods=['POST'])
-def range():
+@app.route('/ranges', methods=['POST'])
+def ranges():
     # Retrieving data from the request form
     arg = request.form.get('name')
     args = arg.split(',')
+    result = None
+    if len(args) < 2:
+        return redirect(url_for('index'))
+    args[0] = args[0].lower()
     
     # SQL query to retrieve earthquakes within the specified time and magnitude range
     query = '''SELECT *
     FROM earthquakes
-    WHERE time BETWEEN {} AND {}
-    AND mag BETWEEN {} AND {}
+    WHERE magSource = {}
     AND type = {}
     '''
-    result = None
-    dfinish = "'2023-05-10T23:20:14.660Z'" # Enter your desired start date/time in the string
-    dstart = "'2023-05-10T20:20:14.660Z'"
-    
-    # Executing the SQL query
-    result = pd.read_sql_query(query.format(args[0], args[1], float(args[2]), float(args[3]), typ), engine)
+    times = []
+    for i in range(int(args[1])):
+        start = time.time()
+        result = pa.default_serialization_context()
+        res = r.get(args[0])
+        if res is not None:
+            result.deserialize(res)
+        else:
+            # Executing the SQL query
+            result = pd.read_sql_query(query.format("'"+args[0]+"'", typ), engine)
+            r.set(args[0],pa.default_serialization_context().serialize(result).to_buffer().to_pybytes())
+        finish = time.time()
+        times.append(args[0]+": "+str(finish-start))
     
     # Rendering the template with the query result
     if result is not None:
-       return render_template('count.html', name = len(result), tables=[result.to_html(classes='data', header="true")])
+       return render_template('count.html', name = times)
     else:
        print('Request for count page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
@@ -123,19 +153,74 @@ def range():
 @app.route('/count', methods=['POST'])
 def count():
     # SQL query to retrieve earthquakes of a specific type and magnitude greater than a threshold
+    
     query = '''SELECT *
     FROM earthquakes
     WHERE type = {}
     AND
-    mag > {}
+    idx = {}
     '''
     result = None
     arg = request.form.get('name')
-    result = pd.read_sql_query(query.format(typ, float(arg)), engine)
-    
+    if not arg.isnumeric():
+        return redirect(url_for('index'))
+    arg = int(arg)
+    rands = []
+    times = []
+    tot = 0
+    for i in range(arg):
+        rands.append(random.randint(0,10000))
+    for i in rands:
+        start = time.time()
+        result = pa.default_serialization_context()
+        res = r.get(i)
+        if res is not None:
+            result.deserialize(res)
+        else:
+            result = pd.read_sql_query(query.format(typ, i), engine)
+            r.set(i,pa.default_serialization_context().serialize(result).to_buffer().to_pybytes())
+        finish = time.time()
+        times.append(str(i)+": "+str(finish-start))
+        tot += (finish-start)
     # Rendering the template with the query result
+    
     if result is not None:
-       return render_template('count.html', name = len(result), tables=[result.to_html(classes='data', header="true")])
+       return render_template('count.html',total = "Total Time: "+str(tot), name = times)
+    else:
+       print('Request for count page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+       
+       
+@app.route('/reg', methods=['POST'])
+def reg():
+    # SQL query to retrieve earthquakes of a specific type and magnitude greater than a threshold
+    
+    query = '''SELECT *
+    FROM earthquakes
+    WHERE type = {}
+    AND
+    idx = {}
+    '''
+    result = None
+    arg = request.form.get('name')
+    if not arg.isnumeric():
+        return redirect(url_for('index'))
+    arg = int(arg)
+    rands = []
+    times = []
+    tot = 0
+    for i in range(arg):
+        rands.append(random.randint(0,10000))
+    for i in rands:
+        start = time.time()
+        result = pd.read_sql_query(query.format(typ, i), engine)
+        finish = time.time()
+        times.append(str(i)+": "+str(finish-start))
+        tot += (finish-start)
+    # Rendering the template with the query result
+    
+    if result is not None:
+       return render_template('count.html',total = "Total Time: "+str(tot), name = times)
     else:
        print('Request for count page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
